63834a4b980c38bbaa6226284190fa5c999bc19c
Raul Cardoso <cardoso.cavalcante@gmail.com>
2023-05-29 22:06:14 -0300

    perf: Passar para a implementacao da api um scopo de coroutines para tirar da thread principal

--- app/src/main/java/com/example/projectteste/repositories/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/projectteste/repositories/UserRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.example.projectteste.repositories
 
 import com.example.projectteste.api.UserApi
 import com.example.projectteste.models.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class UserRepositoryImpl(
     private val userAPI: UserApi
 ) : UserRepository {
-
-    override suspend fun getAllUsers(): List<User> = userAPI.getAllUsers()
+    override suspend fun getAllUsers(): List<User> =
+        withContext(Dispatchers.IO){
+            userAPI.getAllUsers()
+        }
 
 }
